8989fa0f8ba5df14ef1b1f1d089ade18281c51a7
Sheng-Loong Su <sushengloong@gmail.com>
2012-11-17 19:11:40 +0800

    Displays all friends with birthday in the recent months

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,15 @@
 module ApplicationHelper
   def display_month_with_increment(inc=nil)
+    month = sanitize_month(inc)
+    Date::MONTHNAMES[month]
+  end
+
+  def sanitize_month(inc=nil)
     month = Date.current.month
     if inc.present?
       month += inc
       month = (month+1)%13 if month > 12
     end
-    Date::MONTHNAMES[month]
+    month
   end
 end
